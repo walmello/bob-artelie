eba0e8ca65274a8b72863f9868409d7e4e882b10
Walter Melo <41174751+walmello@users.noreply.github.com>
2026-01-16 11:54:50 -0300

    trying to make it work

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.resolve('../.env') });
 
 // https://astro.build/config
 export default defineConfig({});
