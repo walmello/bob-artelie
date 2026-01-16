ef00c0c54b25e701c4b3bbba9ad8136f39b4bd8a
Walter Melo <41174751+walmello@users.noreply.github.com>
2026-01-16 12:11:32 -0300

    adding site urls

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,9 +1,4 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import dotenv from 'dotenv';
-import path from 'path';
-
-dotenv.config({ path: path.resolve('../.env') });
-
 // https://astro.build/config
 export default defineConfig({});
